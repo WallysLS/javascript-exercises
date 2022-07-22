d03932882c6c70c67ca03749691ce2d9f341decd
WallysLS <wallys31.souza@gmail.com>
2022-07-22 10:43:51 -0300

    created letAndConstant, both an easy version and a harder one

---
+++ letAndConst.js
@@ -0,0 +1,12 @@
+function main() {
+    
+    const PI = Math.PI;
+    const r = window. prompt();
+    const area = PI * (r * r);
+    const perimeter = 2 * PI * r;
+    
+    console.log(area);
+    console.log(perimeter);
+    }
+    
+    main();
